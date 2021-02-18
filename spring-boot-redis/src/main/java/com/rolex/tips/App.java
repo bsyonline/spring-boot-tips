f2a7efa2c56a97510ece9b68652b6833b79c4297
zengronghui <zengronghui@kingsoft.com>
2021-02-18 10:51:56 +0800

    spring boot redis

--- spring-boot-redis/src/main/java/com/rolex/tips/App.java
+++ spring-boot-redis/src/main/java/com/rolex/tips/App.java
@@ -1,8 +1,19 @@
+/*
+ * Copyright (C) 2021 bsyonline
+ */
 package com.rolex.tips;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author rolex
+ * @since 2020
+ */
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
