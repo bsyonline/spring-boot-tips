8f770d95d44a44907f778a76c4f3556387869621
zengronghui <zengronghui@kingsoft.com>
2021-03-04 10:59:16 +0800

    spring boot security

--- spring-boot-security/sql/security.sql
+++ spring-boot-security/sql/security.sql
@@ -12,13 +12,16 @@ CREATE TABLE `t_permission`  (
   `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `parent_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+);
 
 -- ----------------------------
 -- Records of t_permission
 -- ----------------------------
-INSERT INTO `t_permission` VALUES (1, '订单列表', 'order_list', '/api/orders', NULL);
-INSERT INTO `t_permission` VALUES (2, '产品列表', 'product_list', '/api/products', NULL);
+INSERT INTO `t_permission` VALUES (1, '首页', 'index', '/', NULL);
+INSERT INTO `t_permission` VALUES (2, '产品管理', 'product_manager', '/products', NULL);
+INSERT INTO `t_permission` VALUES (3, '订单管理', 'order_manager', '/orders', NULL);
+INSERT INTO `t_permission` VALUES (4, '用户管理', 'user_manager', '/sys/users', NULL);
+INSERT INTO `t_permission` VALUES (5, '日志管理', 'log_manager', '/sys/logs', NULL);
 
 -- ----------------------------
 -- Table structure for t_role
@@ -29,7 +32,7 @@ CREATE TABLE `t_role`  (
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `code` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+);
 
 -- ----------------------------
 -- Records of t_role
@@ -46,14 +49,19 @@ CREATE TABLE `t_role_permission`  (
   `role_id` int(11) NOT NULL,
   `permission_id` int(11) NOT NULL,
   PRIMARY KEY (`role_id`, `permission_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+);
 
 -- ----------------------------
 -- Records of t_role_permission
 -- ----------------------------
 INSERT INTO `t_role_permission` VALUES (1, 1);
 INSERT INTO `t_role_permission` VALUES (1, 2);
+INSERT INTO `t_role_permission` VALUES (1, 3);
+INSERT INTO `t_role_permission` VALUES (1, 4);
+INSERT INTO `t_role_permission` VALUES (1, 5);
+INSERT INTO `t_role_permission` VALUES (2, 1);
 INSERT INTO `t_role_permission` VALUES (2, 2);
+INSERT INTO `t_role_permission` VALUES (2, 3);
 INSERT INTO `t_role_permission` VALUES (3, 1);
 
 -- ----------------------------
@@ -69,14 +77,14 @@ CREATE TABLE `t_user`  (
   `password_expired` int(1) DEFAULT 0,
   `account_locked` int(1) DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+);
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
 INSERT INTO `t_user` VALUES (1, 'admin', '$2a$10$/EW4fX9gO9s9MmgDtPZMtePyHLOXSfyaDK3bf7t/lmtZtIoAb/Jea', 1, 0, 0, 0);
-INSERT INTO `t_user` VALUES (2, 'tom', '$2a$10$r6mUBJETz904kr7UOTanbeo93WCa49VO5QukwT6wCLeZxKvWhr7X2', 1, 0, 0, 0);
-INSERT INTO `t_user` VALUES (3, 'alice', '$2a$10$MxBdy53FSHB34obOOOY7k.IXVMoWvzCPhWba3TzkjX8sr0bebP96G', 1, 0, 0, 0);
+INSERT INTO `t_user` VALUES (2, 'John', '$2a$10$r6mUBJETz904kr7UOTanbeo93WCa49VO5QukwT6wCLeZxKvWhr7X2', 1, 0, 0, 0);
+INSERT INTO `t_user` VALUES (3, 'Alice', '$2a$10$MxBdy53FSHB34obOOOY7k.IXVMoWvzCPhWba3TzkjX8sr0bebP96G', 1, 0, 0, 0);
 
 -- ----------------------------
 -- Table structure for t_user_role
@@ -86,7 +94,7 @@ CREATE TABLE `t_user_role`  (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`, `role_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+);
 
 -- ----------------------------
 -- Records of t_user_role
