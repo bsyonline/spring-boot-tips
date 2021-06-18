cea2cc52e14b12c1824bcc12f42df0f01bc339bd
zengronghui <zengronghui@kingsoft.com>
2021-06-18 11:11:49 +0800

    spring boot rabbitmq

--- spring-boot-rabbitmq/spring-boot-rabbitmq-exchange-example/rabbit-producer/src/test/java/com/rolex/tips/service/RabbitProducerTest.java
+++ spring-boot-rabbitmq/spring-boot-rabbitmq-exchange-example/rabbit-producer/src/test/java/com/rolex/tips/service/RabbitProducerTest.java
@@ -1,14 +1,17 @@
 package com.rolex.tips.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * @author rolex
  * @since 2020
  */
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class RabbitProducerTest {
 
     @Autowired
